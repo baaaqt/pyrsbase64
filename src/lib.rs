2c17c6701776c9297b77c9fc44ca251e9bb3a215
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-09-23 02:08:18 +0700

    feat: enhance b64encode and b64decode functions to support alternative characters

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use base64::engine::general_purpose::STANDARD as base64_standard;
 use base64::engine::Config;
 use base64::{encoded_len, Engine};
 use pyo3::buffer::PyBuffer;
-use pyo3::exceptions::{PyBufferError, PyValueError};
+use pyo3::exceptions::{PyBufferError, PyTypeError, PyValueError};
 use pyo3::prelude::*;
 
 fn slice_from_py_any<'a>(py_obj: &Bound<'a, PyAny>) -> PyResult<&'a [u8]> {
@@ -29,14 +29,30 @@ fn slice_from_py_any<'a>(py_obj: &Bound<'a, PyAny>) -> PyResult<&'a [u8]> {
     }
 
     // SAFETY: We have verified that the buffer is contiguous and non-empty.
-    let bytes = unsafe { std::slice::from_raw_parts(ptr as *const u8, buf.len_bytes()) };
+    let bytes = unsafe { std::slice::from_raw_parts(ptr as *const u8, buf.item_count()) };
 
     Ok(bytes)
 }
 
+fn validate_altchars(altchars: &[u8]) -> PyResult<(u8, u8)> {
+    if altchars.len() != 2 {
+        return Err(PyErr::new::<PyValueError, _>(
+            "altchars must be a bytes-like object of length 2",
+        ));
+    }
+    Ok((altchars[0], altchars[1]))
+}
+
 #[pyfunction]
-fn b64encode(s: &Bound<'_, PyAny>) -> PyResult<Vec<u8>> {
+#[pyo3(signature = (s, altchars=None))]
+fn b64encode(s: &Bound<'_, PyAny>, altchars: Option<&Bound<'_, PyAny>>) -> PyResult<Vec<u8>> {
     let bytes = slice_from_py_any(s)?;
+    let altchars = if let Some(alt) = altchars {
+        let alt_bytes = slice_from_py_any(alt)?;
+        Some(validate_altchars(alt_bytes)?)
+    } else {
+        None
+    };
 
     let size = encoded_len(bytes.len(), base64_standard.config().encode_padding()).map_or(
         Err(PyErr::new::<PyValueError, _>(
@@ -49,11 +65,73 @@ fn b64encode(s: &Bound<'_, PyAny>) -> PyResult<Vec<u8>> {
     base64_standard
         .encode_slice(bytes, &mut buf)
         .map_err(|e| PyErr::new::<PyValueError, _>(format!("Base64 encoding error: {}", e)))?;
+
+    if let Some(chars) = altchars {
+        for byte in &mut buf {
+            if *byte == b'+' {
+                *byte = chars.0;
+            } else if *byte == b'/' {
+                *byte = chars.1;
+            }
+        }
+    }
+
     Ok(buf)
+}
+
+#[pyfunction]
+#[pyo3(signature = (s, altchars=None))]
+fn b64decode(s: &Bound<'_, PyAny>, altchars: Option<&Bound<'_, PyAny>>) -> PyResult<Vec<u8>> {
+    let bytes = if let Ok(s) = s.extract::<&str>() {
+        s.as_bytes()
+    } else if let Ok(s) = slice_from_py_any(s) {
+        s
+    } else {
+        return Err(PyErr::new::<PyTypeError, _>(format!(
+            "argument should be a bytes-like object or ASCII string, not '{}'",
+            s.get_type().name()?
+        )));
+    };
+
+    let altchars = if let Some(altchars) = altchars {
+        if let Ok(altchars) = altchars.extract::<&str>() {
+            Some(validate_altchars(altchars.as_bytes())?)
+        } else if let Ok(altchars) = slice_from_py_any(altchars) {
+            Some(validate_altchars(altchars)?)
+        } else {
+            return Err(PyErr::new::<PyTypeError, _>(format!(
+                "altchars should be a bytes-like object or ASCII string, not '{}'",
+                altchars.get_type().name()?
+            )));
+        }
+    } else {
+        None
+    };
+
+    let bytes = if let Some((c1, c2)) = altchars {
+        let mut modified = bytes.to_vec();
+        for byte in &mut modified {
+            if *byte == c1 {
+                *byte = b'+';
+            } else if *byte == c2 {
+                *byte = b'/';
+            }
+        }
+        modified
+    } else {
+        bytes.to_vec()
+    };
+
+    base64_standard
+        .decode(&bytes)
+        .map_err(|e: base64::DecodeError| {
+            PyErr::new::<PyValueError, _>(format!("Base64 decoding error: {}", e))
+        })
 }
 
 #[pymodule]
 fn pyrsbase64(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(b64encode, m)?)?;
+    m.add_function(wrap_pyfunction!(b64decode, m)?)?;
     Ok(())
 }
