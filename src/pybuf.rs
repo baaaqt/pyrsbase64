28064d99981b0e8fedb5da447e3f12aba6b1a1db
Bakyt Niiazaliev <bakyt.worker@gmail.com>
2025-09-25 22:46:46 +0700

    feat: add PyReadableBinaryIO and PyWriteableBinaryIO types to support python IO[bytes] types

--- src/pybuf.rs
+++ src/pybuf.rs
@@ -1,4 +1,5 @@
 use pyo3::{buffer::PyBuffer, exceptions::PyBufferError, prelude::*};
+use pyo3_file::PyFileLikeObject;
 
 pub fn convert_pybytebuf_to_slice<'py>(obj: &Bound<'py, PyAny>) -> PyResult<&'py [u8]> {
     let buf = PyBuffer::<u8>::get(obj)?;
@@ -27,4 +28,44 @@ pub fn convert_pybytebuf_to_slice<'py>(obj: &Bound<'py, PyAny>) -> PyResult<&'py
     let bytes = unsafe { std::slice::from_raw_parts(ptr as *const u8, buf.item_count()) };
 
     Ok(bytes)
+}
+
+pub struct PyReadableBinaryIO(PyFileLikeObject);
+
+impl PyReadableBinaryIO {
+    pub fn read(&self, py: Python<'_>, buf: &mut [u8]) -> PyResult<usize> {
+        self.0.py_read(py, buf).map_err(PyErr::from)
+    }
+}
+
+impl<'py> FromPyObject<'py> for PyReadableBinaryIO {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        Ok(PyReadableBinaryIO(PyFileLikeObject::py_with_requirements(
+            ob.clone(),
+            true,
+            false,
+            false,
+            false,
+        )?))
+    }
+}
+
+pub struct PyWriteableBinaryIO(PyFileLikeObject);
+
+impl PyWriteableBinaryIO {
+    pub fn write(&self, py: Python<'_>, buf: &[u8]) -> PyResult<usize> {
+        self.0.py_write(py, buf).map_err(PyErr::from)
+    }
+}
+
+impl<'py> FromPyObject<'py> for PyWriteableBinaryIO {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        Ok(PyWriteableBinaryIO(PyFileLikeObject::py_with_requirements(
+            ob.clone(),
+            false,
+            true,
+            false,
+            false,
+        )?))
+    }
 }
